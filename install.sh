1cd5f45f1bea1cfcd1ccd58220a944880a9bc699
Federico Grandi <fgrandi30@gmail.com>
2023-09-21 22:08:26 +0200

    feat: set default branch name to `main`

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ git config --global tag.gpgSign true
 git config --global fetch.prune true
 git config --global core.editor vi
 git config --global --add --bool push.autoSetupRemote true
+git config --global init.defaultBranch main
